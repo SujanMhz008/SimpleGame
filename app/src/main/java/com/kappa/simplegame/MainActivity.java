2237d65986718eff4de99a45350ba50ae11d6a40
Sujan Maharjan <zexcu008@gmail.com>
2019-09-26 02:54:41 +0545

    Fixed the OnClick not working Made the numbers refresh each time on click

--- app/src/main/java/com/kappa/simplegame/MainActivity.java
+++ app/src/main/java/com/kappa/simplegame/MainActivity.java
@@ -2,10 +2,13 @@ package com.kappa.simplegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -14,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnNum1, btnNum2;
     private TextView txvResult;
     Random rnNum = new Random();
-    int rndNum1, rndNum2, Num1, Num2, Result;
+    int rndNum, Num1, Num2, Result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +28,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnNum2 = findViewById(R.id.btnNum2);
         txvResult = findViewById(R.id.txvResult);
 
+        randomNumber();
+        btnNum1.setText(Integer.toString(rndNum));
 
-        rndNum1 = rnNum.nextInt(100);
-
-        btnNum1.setText(Integer.toString(rndNum1));
-
-        rndNum2 = rnNum.nextInt(100);
-
-        btnNum2.setText(Integer.toString(rndNum2));
+        randomNumber();
+        btnNum2.setText(Integer.toString(rndNum));
 
         btnNum1.setOnClickListener(this);
         btnNum2.setOnClickListener(this);
+
+        Result = Integer.parseInt(txvResult.getText().toString());
+        if (Result >= 10){
+            //Intent win = new Intent();
+            Toast.makeText(MainActivity.this, "YOU WON!!!", Toast.LENGTH_LONG).show();
+        }
+        else if (Result <= -10){
+            //Intent win = new Intent();
+            Toast.makeText(MainActivity.this, "YOU LOST!!!", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -45,24 +55,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Num2 = Integer.parseInt(btnNum2.getText().toString());
             Result = Integer.parseInt(txvResult.getText().toString());
             if (Num1 > Num2) {
-                Result = Result++;
+                Result = Result+1;
+                txvResult.setText(Integer.toString(Result));
+            } else if (Num2 > Num1) {
+                Result = Result-1;
                 txvResult.setText(Integer.toString(Result));
             }
-            else if (Num2 > Num1) {
-                Result = Result--;
-                txvResult.setText(Integer.toString(Result));
-            }
-        } else if (v.getId() == R.id.btnNum2) {
+
+        }
+        else if (v.getId() == R.id.btnNum2) {
             Num1 = Integer.parseInt(btnNum1.getText().toString());
             Num2 = Integer.parseInt(btnNum2.getText().toString());
             Result = Integer.parseInt(txvResult.getText().toString());
             if (Num2 > Num1) {
-                Result = Result++;
+                Result = Result+1;
                 txvResult.setText(Integer.toString(Result));
-            } else if (Num1 > Num2) {
-                Result = Result--;
+            }
+            else if (Num1 > Num2) {
+                Result = Result-1;
                 txvResult.setText(Integer.toString(Result));
             }
         }
+
+        randomNumber();
+        btnNum1.setText(Integer.toString(rndNum));
+
+        randomNumber();
+        btnNum2.setText(Integer.toString(rndNum));
+
+        if (Result >= 10){
+            Intent win = new Intent(MainActivity.this, WinActivity.class);
+            startActivity(win);
+            //Toast.makeText(MainActivity.this, "YOU WON!!!", Toast.LENGTH_LONG).show();
+        }
+        else if (Result <= -10){
+            Intent lose = new Intent(MainActivity.this, LoseActivity.class);
+            startActivity(lose);
+            //Toast.makeText(MainActivity.this, "YOU LOST!!!", Toast.LENGTH_LONG).show();
+        }
+    }
+    private int randomNumber(){
+        rndNum = rnNum.nextInt(99)+1;
+        return rndNum;
     }
 }
---
+++ app/src/main/java/com/kappa/simplegame/WinActivity.java
@@ -0,0 +1,14 @@
+package com.kappa.simplegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win);
+    }
+}
